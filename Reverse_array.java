79d83bc31135af682069d9439406f91a35205793
Muhammad Irfan <irfanansi3866@gmail.com>
2025-10-13 14:48:03 +0530

    Create Reverse_array.java

---
+++ Reverse_array.java
@@ -0,0 +1,25 @@
+public class reverse_Array{
+    public static void reverse(int []arr){
+        int first=0;int last=arr.length-1;
+        while(first<last){
+            int temp=arr[last];
+            arr [last]=arr[first];
+            arr [first]=temp;
+            first++;
+            last--;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[]args){
+        int []arr={2,4,6,8,10};
+        reverse(arr);
+    }
+}
+
+
+
+
+
+
